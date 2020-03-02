de124677140ee192c8ccd2897e3e6b2544ec5ca4
Dmytro Sokolovskyi <Sokolovskiy01@gmail.com>
2020-03-02 21:05:22 +0100

    Test button add

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace PcGameChecker
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Lorem ipsum dolor sit amet.";
+        }
     }
 }
