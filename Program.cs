f88353240bfbf193344140f613a4581b73e291ca
Zagurskiy Vladyslav <magician.vz17@outlook.com>
2020-03-02 20:07:11 +0100

    Dobavlenie

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace PcGameChecker
         [STAThread]
         static void Main()
         {
+            int x = 0;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
