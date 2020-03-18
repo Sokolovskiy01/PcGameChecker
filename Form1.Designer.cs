abba93cf975442086de71be3c7e5d63d6309cc40
Dmytro Sokolovskyi <Sokolovskiy01@gmail.com>
2020-03-18 15:37:42 +0100

    CpuInfoTextBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(270, 323);
+            this.button1.Location = new System.Drawing.Point(990, 288);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(260, 115);
             this.button1.TabIndex = 0;
@@ -59,23 +60,23 @@
             this.circularProgressBar1.AnimationSpeed = 500;
             this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
             this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
-            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.Transparent;
             this.circularProgressBar1.InnerMargin = 2;
-            this.circularProgressBar1.InnerWidth = -1;
-            this.circularProgressBar1.Location = new System.Drawing.Point(270, 57);
+            this.circularProgressBar1.InnerWidth = 1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(990, 12);
             this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
             this.circularProgressBar1.Name = "circularProgressBar1";
-            this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.DeepPink;
             this.circularProgressBar1.OuterMargin = -25;
             this.circularProgressBar1.OuterWidth = 26;
-            this.circularProgressBar1.ProgressColor = System.Drawing.Color.DodgerBlue;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.Goldenrod;
             this.circularProgressBar1.ProgressWidth = 25;
             this.circularProgressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
             this.circularProgressBar1.Size = new System.Drawing.Size(260, 260);
             this.circularProgressBar1.StartAngle = 270;
-            this.circularProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.circularProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
             this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
             this.circularProgressBar1.SubscriptText = ".23";
@@ -84,13 +85,22 @@
             this.circularProgressBar1.SuperscriptText = "°C";
             this.circularProgressBar1.TabIndex = 2;
             this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
-            this.circularProgressBar1.Value = 68;
+            this.circularProgressBar1.Value = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(972, 623);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1262, 664);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.circularProgressBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -107,6 +117,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,7 +26,25 @@ namespace PcGameChecker
             circularProgressBar1.Minimum = 0;
             circularProgressBar1.Maximum = 100;
             var result = Registry.GetValue("HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", "~MHz", 0);
-            label1.Text = "Processor count : " + Environment.ProcessorCount.ToString() + "\nProcessor frequency: " + ((int)result).ToString() + " MHz";
+            label1.Text = "Processor count : " + Environment.ProcessorCount.ToString() + "\nProcessor frequency: " + ((int)result).ToString() + " MHz\n";
+            ManagementClass myManagementClass = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection myManagementCollection = myManagementClass.GetInstances();
+            PropertyDataCollection myProperties = myManagementClass.Properties;
+            //Dictionary myPropertyResults = new Dictionary();
+
+            foreach (var obj in myManagementCollection)
+            {
+                foreach (var myProperty in myManagementClass.Properties)
+                {
+                    //myPropertyResults.Add(myProperty.Name,obj.Properties[myProperty.Name].Value);
+                    richTextBox1.Text += myProperty.Name + " : " + obj.Properties[myProperty.Name].Value + "\n";
+                }
+            }
+
+            //foreach (var myPropertyResult in myPropertyResults)
+            //{
+            //    Console.WriteLine("{0}: {1}", myPropertyResult.Key, myPropertyResult.Value);
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -34,7 +52,6 @@ namespace PcGameChecker
             //label1.Text = "Lorem ipsum dolor sit amet.";
             circularProgressBar1.Value = 0;
             for (int i = 0; i <= 100; i++){
-                for (int j = 0; j < 400000; j++) ;
                 circularProgressBar1.Value = i;
             }
         }
